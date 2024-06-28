4cbda95ad81e6dbf5ba97d9b76c1ea7891fa0843
Alex Johnston <alexmjohnston1@gmail.com>
2024-06-28 11:43:07 -0700

    Add telescope mappings

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -85,6 +85,10 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
+      vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = '[B]uffers' })
+      vim.keymap.set('n', '<leader>sp', builtin.git_files, { desc = '[S]earch [P]roject Files' })
+      vim.keymap.set('n', '<leader>o', builtin.git_files, { desc = '[O]pen Project File' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -92,6 +96,7 @@ return {
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
           previewer = false,
+          layout_config = { width = 112 },
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
